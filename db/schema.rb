f786c649a67d8947e27157267892228f2d97dc15
yujiro11040827 <71250516+yujiro11040827@users.noreply.github.com>
2020-11-09 21:30:01 +0900

    productsテーブル作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_09_121158) do
+ActiveRecord::Schema.define(version: 2020_11_09_122250) do
+
+  create_table "products", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
+    t.string "name"
+    t.string "category"
+    t.integer "price"
+    t.string "seller"
+    t.string "status"
+    t.integer "shipment_source"
+    t.integer "date_shipment"
+    t.string "image"
+    t.string "explanation"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
     t.string "name"
