c43fa9db9de280767bc9724cd51a93531cb3dc97
yujiro11040827 <71250516+yujiro11040827@users.noreply.github.com>
2020-11-12 19:16:49 +0900

    protectedを正しく修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
-  prorected
+  protected
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(
