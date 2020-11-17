8e9efbbfea5011fbac2fa94e40d677e6f90c2225
yujiro11040827 <71250516+yujiro11040827@users.noreply.github.com>
2020-11-18 01:48:08 +0900

    productsテーブル作成（カラム修正）

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,15 @@
 ActiveRecord::Schema.define(version: 2020_11_13_002441) do
 
   create_table "products", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name", null: false
+    t.integer "user_id"
+    t.integer "genre_id", null: false
+    t.integer "price", null: false
+    t.integer "status_id", null: false
+    t.integer "shipment_charge_id", null: false
+    t.integer "shipment_source_id", null: false
+    t.integer "date_shipment_id", null: false
+    t.text "explanation", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
---
+++ spec/factories/shipment_charges.rb
@@ -0,0 +1,5 @@
+FactoryBot.define do
+  factory :shipment_charge do
+    
+  end
+end
