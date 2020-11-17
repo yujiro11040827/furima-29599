2e49f7ed43295ba2d8c23bdcfeac8c36f4c35553
yujiro11040827 <71250516+yujiro11040827@users.noreply.github.com>
2020-11-18 01:58:49 +0900

    バリデーション設定

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,8 @@
 class Product < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :user, :genre, :status, :shipment_charge, :shipment_source, :date_shipment
+
+  validates :name, :price, :explanation, presence: true
+
+  validates :genre_id, :status_id, numericality: { other_than: 1 } 
 end
