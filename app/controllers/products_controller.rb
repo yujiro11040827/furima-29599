2b2db5867913b2ea1b328354d49e60fafced8a1f
yujiro11040827 <71250516+yujiro11040827@users.noreply.github.com>
2020-11-19 16:29:41 +0900

    データ保存後の条件分岐

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,6 +1,5 @@
 class ProductsController < ApplicationController
   def index
-    @products = Products.all
   end
 
   def new
@@ -8,11 +7,16 @@ class ProductsController < ApplicationController
   end
 
   def create
+    if
     Product.create(product_params)
+      redirect_to products_path
+    else
+      render :index
+    end
   end
 
   private
   def product_params
-    params.require(:product).permit(:name, :image, :category_id, :price, :status_id, :shipment_charge_id, :shipment_source_id, :date_shipment_id, :explanation).merge(user_id: current_user.id)
+    params.require(:product).permit(:name, :image, :genre_id, :price, :status_id, :shipment_charge_id, :shipment_source_id, :date_shipment_id, :explanation).merge(user_id: current_user.id)
   end
 end
