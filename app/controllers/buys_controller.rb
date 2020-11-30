cba27f35fc7e707d1b7ba50b6e02f908eb1ad950
yujiro11040827 <71250516+yujiro11040827@users.noreply.github.com>
2020-11-30 18:50:02 +0900

    購入機能のベース作成

--- app/controllers/buys_controller.rb
+++ app/controllers/buys_controller.rb
@@ -5,12 +5,13 @@ class BuysController < ApplicationController
       redirect_to  user_session_path
     end
     @product = Product.find(params[:product_id])
+    @buy_destination = BuyDestination.new
   end
 
   def create
     @buy_destination = BuyDestination.new(buy_params)
-    if @buy.valid?
-      @buy.save
+    if @buy_destination.valid?
+      @buy_destination.save
       return redirect_to root_path
     else
       render 'index'
