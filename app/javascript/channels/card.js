4f965adc15af0bf789cf5fe6c24bc364dfbc80a6
yujiro11040827 <71250516+yujiro11040827@users.noreply.github.com>
2020-12-07 17:59:25 +0900

    不要な部分の削除

--- app/javascript/channels/card.js
+++ app/javascript/channels/card.js
@@ -1,6 +1,5 @@
 const pay = () => {
   Payjp.setPublicKey(process.env.PAYJP_PUBLIC_KEY);
-  Payjp.setPublicKey("pk_test_ee789f02ffd29349437755a2");
   const form = document.getElementById("charge-form");
   form.addEventListener("submit", (e) => {
     e.preventDefault();
